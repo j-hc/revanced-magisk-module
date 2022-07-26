e4e2aa943944fd2590302bfe33d15de4f1c95147
j-hc <25510067+j-hc@users.noreply.github.com>
2022-07-26 21:10:13 +0300

    ci: check if patches are updated and build

--- build.sh
+++ build.sh
@@ -30,26 +30,11 @@ fi
 log "$(date +'%Y-%m-%d')\n"
 mkdir -p "$BUILD_DIR"
 
-if [ "$UPDATE_PREBUILTS" = true ]; then
-	get_prebuilts
-else
-	set_prebuilts
-fi
+if [ "$UPDATE_PREBUILTS" = true ]; then get_prebuilts; else set_prebuilts; fi
 
-if [ "$BUILD_YT" = true ]; then
-	build_yt
-fi
-
-if [ "$BUILD_MUSIC_ARM64_V8A" = true ]; then
-	build_music "$ARM64_V8A"
-fi
-
-if [ "$BUILD_MUSIC_ARM_V7A" = true ]; then
-	build_music "$ARM_V7A"
-fi
-
-if [ "$BUILD_TWITTER" = true ]; then
-	build_twitter
-fi
+if [ "$BUILD_YT" = true ]; then build_yt; fi
+if [ "$BUILD_MUSIC_ARM64_V8A" = true ]; then build_music "$ARM64_V8A"; fi
+if [ "$BUILD_MUSIC_ARM_V7A" = true ]; then build_music "$ARM_V7A"; fi
+if [ "$BUILD_TWITTER" = true ]; then build_twitter; fi
 
 echo "Done"
