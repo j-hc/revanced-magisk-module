4d64641476db00c30c2defbf1e435a0b0c296e86
j-hc <25510067+j-hc@users.noreply.github.com>
2022-08-22 15:11:43 +0300

    remove those

--- utils.sh
+++ utils.sh
@@ -19,16 +19,16 @@ get_prebuilts() {
 	echo "Getting prebuilts"
 	RV_CLI_URL=$(req https://api.github.com/repos/j-hc/revanced-cli/releases/latest - | tr -d ' ' | sed -n 's/.*"browser_download_url":"\(.*jar\)".*/\1/p')
 	RV_CLI_JAR="${TEMP_DIR}/${RV_CLI_URL##*/}"
-	log "CLI: [${RV_CLI_JAR#"$TEMP_DIR/"}](https://github.com/j-hc/revanced-cli/releases/latest)"
+	log "CLI: ${RV_CLI_JAR#"$TEMP_DIR/"}"
 
 	RV_INTEGRATIONS_URL=$(req https://api.github.com/repos/revanced/revanced-integrations/releases/latest - | tr -d ' ' | sed -n 's/.*"browser_download_url":"\(.*apk\)".*/\1/p')
 	RV_INTEGRATIONS_APK=${RV_INTEGRATIONS_URL##*/}
 	RV_INTEGRATIONS_APK="${TEMP_DIR}/${RV_INTEGRATIONS_APK%.apk}-$(cut -d/ -f8 <<<"$RV_INTEGRATIONS_URL").apk"
-	log "Integrations: [${RV_INTEGRATIONS_APK#"$TEMP_DIR/"}](https://github.com/revanced/revanced-integrations/releases/latest)"
+	log "Integrations: ${RV_INTEGRATIONS_APK#"$TEMP_DIR/"}"
 
 	RV_PATCHES_URL=$(req https://api.github.com/repos/revanced/revanced-patches/releases/latest - | tr -d ' ' | sed -n 's/.*"browser_download_url":"\(.*jar\)".*/\1/p')
 	RV_PATCHES_JAR="${TEMP_DIR}/${RV_PATCHES_URL##*/}"
-	log "Patches: [${RV_PATCHES_JAR#"$TEMP_DIR/"}](https://github.com/revanced/revanced-patches/releases/latest)"
+	log "Patches: ${RV_PATCHES_JAR#"$TEMP_DIR/"}"
 
 	dl_if_dne "$RV_CLI_JAR" "$RV_CLI_URL"
 	dl_if_dne "$RV_INTEGRATIONS_APK" "$RV_INTEGRATIONS_URL"
