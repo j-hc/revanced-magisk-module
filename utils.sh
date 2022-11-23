d6fbf67e98e56c92f20a112d5c1d8f3b5a302cb1
j-hc <25510067+j-hc@users.noreply.github.com>
2022-11-23 19:45:14 +0300

    merge integrations for twitch

--- utils.sh
+++ utils.sh
@@ -343,6 +343,7 @@ build_reddit() {
 build_twitch() {
 	declare -A twitch_args
 	twitch_args[app_name]="Twitch"
+	twitch_args[patcher_args]="-m ${RV_INTEGRATIONS_APK}"
 	twitch_args[mode]="$TWITCH_MODE"
 	twitch_args[pkg_name]="tv.twitch.android.app"
 	twitch_args[apkmirror_dlurl]="twitch-interactive-inc/twitch"
