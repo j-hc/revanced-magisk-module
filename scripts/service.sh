2ba59e9230f5c3e4b6c77b13e13b7152402a4aca
j-hc <j-hc@users.noreply.github.com>
2022-12-26 19:48:12 +0300

    check the validity of installation before mounting

--- scripts/service.sh
+++ scripts/service.sh
@@ -8,7 +8,7 @@ sleep __MNTDLY
 ln -f $MODDIR/base.apk $RVPATH
 BASEPATH=$(pm path __PKGNAME | grep base)
 BASEPATH=${BASEPATH#*:}
-if [ $BASEPATH ]; then
+if [ $BASEPATH ] && [ -d ${BASEPATH%base.apk}lib ]; then
 	VERSION=$(dumpsys package __PKGNAME | grep versionName)
 	if [ ${VERSION#*=} = __PKGVER ]; then
 		chcon u:object_r:apk_data_file:s0 $RVPATH
