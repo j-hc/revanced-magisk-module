4cd58ebc0708af65a4fce6c3d0285fa3d94532ef
j-hc <25510067+j-hc@users.noreply.github.com>
2023-01-06 22:45:51 +0300

    Update service.sh

--- scripts/service.sh
+++ scripts/service.sh
@@ -5,19 +5,18 @@ RVPATH=/data/adb/__PKGNAME_rv.apk
 until [ "$(getprop sys.boot_completed)" = 1 ]; do sleep 1; done
 until [ -d /sdcard ]; do sleep 1; done
 
-grep __PKGNAME /proc/self/mountinfo | while read -r line; do
-	mountpoint=$(echo "$line" | cut -d' ' -f5)
-	umount -l "${mountpoint%%\\*}"
-done
-am force-stop __PKGNAME
-
 ln -f $MODDIR/base.apk $RVPATH
 BASEPATH=$(pm path __PKGNAME | grep base)
 BASEPATH=${BASEPATH#*:}
 if [ $BASEPATH ] && [ -d ${BASEPATH%base.apk}lib ]; then
 	VERSION=$(dumpsys package __PKGNAME | grep -m1 versionName)
 	if [ ${VERSION#*=} = __PKGVER ]; then
+		grep __PKGNAME /proc/self/mountinfo | while read -r line; do
+		        mountpoint=$(echo "$line" | cut -d' ' -f5)
+		        umount -l "${mountpoint%%\\*}"
+		done
 		chcon u:object_r:apk_data_file:s0 $RVPATH
 		mount -o bind $RVPATH $BASEPATH
+		am force-stop __PKGNAME
 	fi
 fi
