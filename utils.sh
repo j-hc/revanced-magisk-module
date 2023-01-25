27ab245ab39f191e608274638518abfa4e15ea20
Peter Ardiarta <50816765+prooholic@users.noreply.github.com>
2023-01-25 15:06:40 +0700

    Update utils.sh

--- utils.sh
+++ utils.sh
@@ -365,7 +365,7 @@ module_prop() {
 name=${2}
 version=v${3}
 versionCode=${NEXT_VER_CODE}
-author=j-hc
+author=0xTaaa
 description=${4}" >"${6}/module.prop"
 
 	if [ "$ENABLE_MAGISK_UPDATE" = true ]; then echo "updateJson=${5}" >>"${6}/module.prop"; fi
