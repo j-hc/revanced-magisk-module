ee7e02a3a32de497238ebb9fdfa54d4110b9ac60
j-hc <j-hc@users.noreply.github.com>
2023-02-12 22:15:29 +0300

    update module scripts

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -5,10 +5,4 @@
 	NVBASE=${MODULES%/*}
 	rm $NVBASE/rvhc/__PKGNAME_rv.apk
 	rmdir $NVBASE/rvhc
-	if __ISBNDL :; then
-		until [ "$(getprop sys.boot_completed)" = 1 ]; do sleep 1; done
-		until [ "$(getprop init.svc.bootanim)" = stopped ]; do sleep 1; done
-		sleep 3
-		pm uninstall __PKGNAME
-	fi
 } &
