3b5e4c2afab5d29d9e12a3a542f12ef17387d470
kevinr99089 <93716347+kevinr99089@users.noreply.github.com>
2023-04-02 13:10:57 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -133,6 +133,7 @@ music_arm64_mode=$(toml_get "$(toml_get_table "Music-arm64")" "build-mode") || m
 if [ "$youtube_mode" != module ] || [ "$music_arm_mode" != module ] || [ "$music_arm64_mode" != module ]; then
 	log "\nInstall [Vanced Microg](https://github.com/inotia00/VancedMicroG/releases/latest) to be able to use non-root YouTube or Music"
 fi
-log "nWant the ReVanced builds? click [here](https://github.com/kevinr99089/revanced.builder/releases/latest)"
+
+log "/nWant the ReVanced builds? click [here](https://github.com/kevinr99089/revanced.builder/releases/latest)"
 
 pr "Done"
