d67e9a1f77983d82dd8865391c02eb255f365001
kevinr99089 <93716347+kevinr99089@users.noreply.github.com>
2023-04-30 14:12:31 +0200

    add option include-stock

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -2,7 +2,7 @@
 {
 	rm $NVBASE/rvhc/__PKGNAME_rv.apk
 	rmdir $NVBASE/rvhc
-	if __ISBNDL :; then
+	if __ISBNDL; then
 		until [ "$(getprop sys.boot_completed)" = 1 ]; do sleep 1; done
 		sleep 15
 		pm uninstall __PKGNAME
