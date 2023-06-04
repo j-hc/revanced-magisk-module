4fd1f18a7d92a375386c109909d043398770dcd9
Kevin Nitro <86353526+KevinNitroG@users.noreply.github.com>
2023-06-04 20:16:25 +0000

    yep

--- build.sh
+++ build.sh
@@ -156,7 +156,7 @@ fi
 if youtube_t=$(toml_get_table "YouTube"); then youtube_mode=$(toml_get "$youtube_t" "build-mode") || youtube_mode="apk"; else youtube_mode="module"; fi
 if music_t=$(toml_get_table "Music"); then music_mode=$(toml_get "$music_t" "build-mode") || music_mode="apk"; else music_mode="module"; fi
 if [ "$youtube_mode" != module ] || [ "$music_mode" != module ]; then
-	log "\nInstall [Vanced Microg](https://github.com/TeamVanced/VancedMicroG/releases) for non-root YouTube or YT Music"
+	log "\nCài [MicroG](https://github.com/inotia00/mMicroG/releases/latest) để dùng non-root Revanced Extended"
 fi
 log "\n[revanced-magisk-module](https://github.com/j-hc/revanced-magisk-module)"
 log "\n---\nChangelog:"
