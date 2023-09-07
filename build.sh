41e990530829c75482107455442858fa8a5ebf5b
joelschmol <88237375+joelschmol@users.noreply.github.com>
2023-09-07 07:58:54 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -36,7 +36,7 @@ DEF_INTEGRATIONS_VER=$(toml_get "$main_config_t" integrations-version) || DEF_IN
 DEF_CLI_VER=$(toml_get "$main_config_t" cli-version) || DEF_CLI_VER=""
 DEF_PATCHES_SRC=$(toml_get "$main_config_t" patches-source) || DEF_PATCHES_SRC="ReVanced/revanced-patches"
 DEF_INTEGRATIONS_SRC=$(toml_get "$main_config_t" integrations-source) || DEF_INTEGRATIONS_SRC="ReVanced/revanced-integrations"
-DEF_CLI_SRC=$(toml_get "$main_config_t" cli-source) || DEF_CLI_SRC="j-hc/revanced-cli"
+DEF_CLI_SRC=$(toml_get "$main_config_t" cli-source) || DEF_CLI_SRC="revanced/revanced-cli"
 DEF_RV_BRAND=$(toml_get "$main_config_t" rv-brand) || DEF_RV_BRAND="ReVanced"
 # -- Main config --
 mkdir -p $TEMP_DIR $BUILD_DIR
