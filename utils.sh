0d92ecf8dcea3ddab7d5b2259e3a45aeac5ca3d8
j-hc <j-hc@users.noreply.github.com>
2023-10-09 23:33:47 +0300

    quotes.

--- utils.sh
+++ utils.sh
@@ -564,7 +564,7 @@ build_rv() {
 	done
 }
 
-list_args() { tr -d '\t\r' <<<"$1" | tr -s ' ' | sed 's/" "/"\n"/g' | grep -v '^$' || :; }
+list_args() { tr -d '\t\r' <<<"$1" | tr -s ' ' | sed 's/" "/"\n"/g' | sed 's/\([^"]\)"\([^"]\)/\1'\''\2/g' | grep -v '^$' || :; }
 join_args() { list_args "$1" | sed "s/^/${2} /" | paste -sd " " - || :; }
 
 uninstall_sh() {
