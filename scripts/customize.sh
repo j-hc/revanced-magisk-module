7ebd9b7523e54df455d45459b8d306f4c40ccf1b
cvnertnc <148134890+cvnertnc@users.noreply.github.com>
2023-11-06 20:56:55 +0300

    `-x-`
    x

--- scripts/customize.sh
+++ scripts/customize.sh
@@ -113,5 +113,12 @@ for s in "uninstall.sh" "service.sh"; do
 done
 
 ui_print "* Done"
-ui_print "  by j-hc (github.com/j-hc)"
+ui_print "  by cvnertnc (github.com/cvnertnc)"
+ui_print "  thanks to ReVanced Team (github.com/ReVanced)"
+ui_print "  thanks to inotia00 (github.com/inotia00)"
+ui_print "  thanks to j-hc (github.com/j-hc)"
 ui_print " "
+
+sleep 0.5
+
+ui_print "  No need to reboot your phone"
