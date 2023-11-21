4ed4133ae832a8dc8bdeeebf820da52ff46cd476
LukaszMech <81306360+LukeMech@users.noreply.github.com>
2023-11-21 21:13:16 +0100

    Update utils.sh

--- utils.sh
+++ utils.sh
@@ -331,14 +331,13 @@ get_aptoide_resp() {
 get_aptoide_vers() { grep -oE '"vername":"[^"]+"' <<<"$__APTOIDE_RESP__" | awk -F'"' '{print $4}'; }
 dl_aptoide() {
 	local url=$1 version=$2 output=$3
- 	local response id
   	if [ -n "$version" ]; then
+    		local response id
      		id=$(grep -oE "\"vername\":\s*\"$version\",\"id\":\s*[0-9]+" <<<"$__APTOIDE_RESP__" | awk -F':' '{print $3}' | head -1) || return 1
        		response=$(req "https://en.aptoide.com/download?app_id=${id}&store_name=aptoide-web" -) || return 1
 		url=$(grep -oE '"path":\s*"[^"]+.apk"' <<<"$response" | awk -F'"' '{print $4}' | head -1) || return 1;
 	else url=""; fi
 	url=$(grep -oE '"path":\s*"[^"]+.apk"' <<<"$__APTOIDE_RESP_PKG__" | head -1 | awk -F'"' '{print $4}') || return 1
- 	pr $url
 	req "$url" "$output"
 }
 get_aptoide_pkg_name() { grep -oE '"h1":\s*"([^"]*)"' <<<"$__APTOIDE_RESP_PKG__" | head -1 | awk -F'"' '{print $4}'; }
