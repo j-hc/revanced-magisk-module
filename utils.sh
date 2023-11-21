495653241bd69f0517bc50bdb5b0d1d5149b712f
LukaszMech <81306360+LukeMech@users.noreply.github.com>
2023-11-21 19:13:21 +0100

    Update utils.sh

--- utils.sh
+++ utils.sh
@@ -414,8 +414,9 @@ build_rv() {
 		for dl_p in archive apkmirror uptodown apkmonk; do
 			if [ -z "${args[${dl_p}_dlurl]}" ]; then continue; fi
 			pr "Downloading '${table}' from ${dl_p}"
-			if ! dl_${dl_p} "${args[${dl_p}_dlurl]}" "$stock_apk" "$arch" "${args[dpi]}"; then
-				epr "ERROR: Could not download '${table}' from ${dl_p} with version '${version}', arch '${arch}', dpi '${args[dpi]}'"
+			if [ "$get_latest_ver" = true ] && { [ "$dl_p" = "uptodown" ] || [ "$dl_p" = "aptoide" ]; } ; then local vver=""; else local vver=$version; fi
+			if ! dl_${dl_p} "${args[${dl_p}_dlurl]}" "$vver" "$stock_apk" "$arch" "${args[dpi]}"; then
+   				epr "ERROR: Could not download '${table}' from ${dl_p} with version '${version}', arch '${arch}', dpi '${args[dpi]}'"
 				continue
 			fi
 			break
