a1083028742ff0c2346bf626320c329b3ef2c049
LukaszMech <81306360+LukeMech@users.noreply.github.com>
2023-11-21 20:52:54 +0100

    Update utils.sh

--- utils.sh
+++ utils.sh
@@ -335,12 +335,12 @@ dl_aptoide() {
   	if [ -n "$version" ]; then
      		id=$(grep -oE "\"vername\":\s*\"$version\",\"id\":[0-9]+" <<<"$__APTOIDE_RESP__" | awk -F':' '{print $3}' | head -1) || return 1
        		pr 'X D'
-       		response=$(req "https://en.aptoide.com/download?app_id=${id}&store_name=aptoide-web") || return 1
+       		response=$(req "https://en.aptoide.com/download?app_id=${id}&store_name=aptoide-web" -) || return 1
 	 	pr 'blabla'
 		url=$(grep -oE '"path":\s*"https[^"]*"' <<<"$response" | awk -F'"' '{print $3}' | head -1) || return 1;
 	else url=""; fi
 	url=$(grep -oE '"path":\s*"https[^"]*"' <<<"$__APTOIDE_RESP_PKG__" | awk -F'"' '{print $3}' | head -1) || return 1
-	req "$url" "$output"
+	req "$url" "$output";
 }
 get_aptoide_pkg_name() { grep -oE '"h1":\s*"([^"]*)"' <<<"$__APTOIDE_RESP_PKG__" | head -1 | awk -F'"' '{print $3}'; }
 # --------------------------------------------------
