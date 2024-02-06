c804afaf0d5d67277b27e9e7d707abb77a6988e3
Mohammed Rabil <rabil@techie.com>
2024-02-07 00:30:14 +0530

    Update CLI URL format in extras.sh

--- extras.sh
+++ extras.sh
@@ -22,7 +22,7 @@ get_rv_prebuilts() {
     if [ "$patches_ver" ]; then rv_patches_rel+="tags/${patches_ver}"; else rv_patches_rel+="latest"; fi
     rv_cli_url=$(gh_req "$rv_cli_rel" - | json_get 'browser_download_url') || return 1
     local rv_cli_jar="${cli_dir}/${rv_cli_url##*/}"
-    echo "CLI: $(cut -d/ -f4 <<<"$rv_cli_url")/$(cut -d/ -f9 <<<"$rv_cli_url")"
+    echo "CLI: $(cut -d/ -f4 <<<"$rv_cli_url")/$(cut -d/ -f9 <<<"$rv_cli_url")  "
 
     rv_integrations_url=$(gh_req "$rv_integrations_rel" - | json_get 'browser_download_url') || return 1
     local rv_integrations_apk="${integrations_dir}/${rv_integrations_url##*/}"
