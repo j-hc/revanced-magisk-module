818e99a23f4e3d3c6be339ef552244dfae0cb359
j-hc <j-hc@users.noreply.github.com>
2024-03-25 00:06:33 +0300

    update scripts

--- scripts/service.sh
+++ scripts/service.sh
@@ -14,7 +14,7 @@ sleep 5
 
 err() {
 	[ ! -f $MODDIR/err ] && cp $MODDIR/module.prop $MODDIR/err
-	sed -i "s/^des.*/description=⚠️ Module is inactive: '${1}'/g" $MODDIR/module.prop
+	sed -i "s/^des.*/description=⚠️ Needs reflash: '${1}'/g" $MODDIR/module.prop
 }
 
 if [ $svcl = 0 ]; then
@@ -40,7 +40,7 @@ if [ $svcl = 0 ]; then
 			err "version mismatch (installed:${VERSION}, module:__PKGVER)"
 		fi
 	else
-		err "invalid installation"
+		err "zygote crashed"
 	fi
 else
 	err "app not installed"
