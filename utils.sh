29c132c2860473f88d15592b62799a6f9dbcc164
E85Addict <77761710+E85Addict@users.noreply.github.com>
2024-03-30 18:18:15 -0700

    fix: Correct directory to stop error.

--- utils.sh
+++ utils.sh
@@ -92,10 +92,10 @@ get_prebuilts() {
 	if [ "$OS" = Android ]; then
 		local arch
 		if [ "$(uname -m)" = aarch64 ]; then arch=arm64; else arch=arm; fi
-		HTMLQ="${BIN_DIR}/htmlq-${arch}"
-		AAPT2="${BIN_DIR}/aapt2-${arch}"
+		HTMLQ="${BIN_DIR}/htmlq/htmlq-${arch}"
+		AAPT2="${BIN_DIR}/aapt2/aapt2-${arch}"
 	else
-		HTMLQ="${BIN_DIR}/htmlq-x86_64"
+		HTMLQ="${BIN_DIR}/htmlq/htmlq-x86_64"
 	fi
 	mkdir -p ${MODULE_TEMPLATE_DIR}/bin/arm64 ${MODULE_TEMPLATE_DIR}/bin/arm ${MODULE_TEMPLATE_DIR}/bin/x86 ${MODULE_TEMPLATE_DIR}/bin/x64
 	dl_if_dne "${MODULE_TEMPLATE_DIR}/bin/arm64/cmpr" "https://github.com/j-hc/cmpr/releases/latest/download/cmpr-arm64-v8a"
