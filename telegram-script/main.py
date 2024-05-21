f923ca996067a07230d85bc7a56b1a40fe8a9536
Mohammed Rabil <mail@rabil.me>
2024-05-21 12:08:38 +0530

    Update main.py

--- telegram-script/main.py
+++ telegram-script/main.py
@@ -32,25 +32,7 @@ def generate_files_message():
 
 
 def fetch_microg():
-
-    vanced_microg_release = requests.get(Config.MICROG_RELEASE_URL).json()
-
-    microg_file = vanced_microg_release["assets"][0] or []
-
-    microg_name = (
-        microg_file["name"].strip(".apk")
-        + "-"
-        + vanced_microg_release["tag_name"]
-        + ".apk"
-        or "microg.apk"
-    )
-    microg_file = microg_file["browser_download_url"] or ""
-
-    if "http" in microg_file:
-        return {"microg_name": microg_name, "microg_file": microg_file}
-    else:
-        # to avoid error returning empty string
-        return {"microg_name": "", "microg_file": ""}
+    return {"microg_name": "GMS_CORE(microg).apk", "microg_file": Config.MICROG_RELEASE_URL}
 
 
 def fetch_changelogs_telegraph_url():
