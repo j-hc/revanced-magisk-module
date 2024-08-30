a9571a651d86a34042c0274a1b7e3da6b0fc4509
j-hc <j-hc@users.noreply.github.com>
2024-08-30 18:44:33 +0300

    module: pm

--- revanced-magisk/customize.sh
+++ revanced-magisk/customize.sh
@@ -38,7 +38,7 @@ done
 am force-stop "$PKG_NAME"
 
 INS=true
-if BASEPATH=$(pm path "$PKG_NAME"); then
+if BASEPATH=$(pm path "$PKG_NAME" 2>&1 </dev/null); then
 	BASEPATH=${BASEPATH##*:} BASEPATH=${BASEPATH%/*}
 	if [ "${BASEPATH:1:6}" = system ]; then
 		ui_print "* $PKG_NAME is a system app"
@@ -90,7 +90,7 @@ install() {
 		abort "$op"
 	fi
 	settings put global verifier_verify_adb_installs 1
-	if BASEPATH=$(pm path "$PKG_NAME"); then
+	if BASEPATH=$(pm path "$PKG_NAME" 2>&1 </dev/null); then
 		BASEPATH=${BASEPATH##*:} BASEPATH=${BASEPATH%/*}
 	else
 		abort "ERROR: install $PKG_NAME manually and reflash the module"
