1f408bfee81f28e8f00c1e8a911dad70370bb145
j-hc <j-hc@users.noreply.github.com>
2024-09-23 22:56:42 +0300

    get_rv_prebuilts

--- utils.sh
+++ utils.sh
@@ -82,13 +82,13 @@ get_rv_prebuilts() {
 			name=$(jq -r .name <<<"$asset")
 			file="${dir}/${name}"
 			gh_dl "$file" "$url" >&2 || return 1
+			if [ "$tag" = "Integrations" ]; then integs_file=$file; fi
 		else
 			name=$(basename "$file")
 			tag_name=$(cut -d'-' -f3- <<<"$name")
 			tag_name=v${tag_name%.*}
 			if [ "$tag_name" = "v" ]; then abort; fi
 		fi
-		if [ "$tag" = "Integrations" ] && [ ! -f "$file" ]; then integs_file=$file; fi
 
 		echo "$tag: $(cut -d/ -f1 <<<"$src")/${name}  " >>"${cl_dir}/changelog.md"
 		echo -n "$file "
@@ -116,10 +116,7 @@ get_rv_prebuilts() {
 			mv -f "${integs_file}-zip/classes-patched.dex" "${integs_file}-zip/classes.dex" || return 1
 			zip -0rq "${integs_file}" . || return 1
 			rm -r "${integs_file}-zip"
-		} >&2 || {
-			epr "Patching revanced-integrations failed"
-			return 1
-		}
+		} >&2 || epr "Patching revanced-integrations failed"
 	fi
 }
 
