998057a4c66a18eaba9cc7189ef5da76a8e03f00
j-hc <j-hc@users.noreply.github.com>
2024-09-23 13:05:51 +0300

    fix get_rv_prebuilts

--- utils.sh
+++ utils.sh
@@ -42,6 +42,7 @@ get_rv_prebuilts() {
 	local cl_dir=${patches_src%/*}
 	cl_dir=${TEMP_DIR}/${cl_dir,,}-rv
 	[ -d "$cl_dir" ] || mkdir "$cl_dir"
+	: >"${cl_dir}/changelog.md"
 	for src_ver in "$cli_src CLI $cli_ver" "$integrations_src Integrations $integrations_ver" "$patches_src Patches $patches_ver"; do
 		set -- $src_ver
 		local src=$1 tag=$2 ver=${3-} ext
