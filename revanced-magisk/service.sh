0982537c1e3545834d0be556dda14198af4a022a
Tesla <43712252+0x11DFE@users.noreply.github.com>
2025-01-25 14:08:05 +0100

    feat: Introduce ksu_susfs to address detected mounts and inconsistent unmounts
    This commit introduces the ksu_susfs module for KernelSU, designed to address issues with detected mounts and inconsistent unmounts, thereby enhancing root hiding capabilities.
    
    Previous versions of KernelSU relied on basic bind mounts and simple unmounting mechanisms, which are often detectable by sophisticated applications and can lead to inconsistent unmounting behavior.
    
    ksu_susfs provides a more robust solution by implementing advanced filesystem manipulation techniques within the kernel. This initial implementation focuses on the following features:
    
    - Spoofing File Metadata (Stat Information):
        - `add_sus_kstat`: Stores the original stat information of a path before it's modified by a bind mount or overlay.
        - `update_sus_kstat`: Updates the inode of a previously added path after it has been mounted over, while preserving the size and block information to maintain consistency with the current stat.
    - Concealing Mounted Paths:
        - `add_sus_mount`: Hides mounted paths from being listed in `/proc/self/[mounts|mountinfo|mountstats]`, preventing applications from easily detecting altered mount points.
    
    These features are integrated into the `service.sh` script to improve the handling of bind mounts:
    
    - Storing original stat information (`add_sus_kstat`) before mounting.
    - Updating the inode (`update_sus_kstat`) after mounting to reflect the new location while maintaining consistency.
    - Hiding the mount point (`add_sus_mount`) from detection.
    
    Benefits:
    
    - Fixes Detected Mounts: ksu_susfs effectively addresses the issue of applications detecting root modifications through mounted filesystems by spoofing metadata and hiding mount points.
    - Resolves Inconsistent Unmounts: The module lays the groundwork for more reliable and consistent unmounting behavior in future updates.
    - Enhanced Root Detection Evasion: By hiding modifications, ksu_susfs significantly improves the ability to evade root detection.

--- revanced-magisk/service.sh
+++ revanced-magisk/service.sh
@@ -3,6 +3,14 @@ MODDIR=${0%/*}
 RVPATH=/data/adb/rvhc/${MODDIR##*/}.apk
 . "$MODDIR/config"
 
+# Define the path of root manager applet bin directories using find and set it to $PATH then export it
+if ! command -v busybox >/dev/null 2>&1; then
+	TOYS_PATH=$(find "/data/adb" -maxdepth 3 \( -name busybox -o -name ksu_susfs \) -exec dirname {} \; | sort -u | tr '\n' ':')
+	export PATH="${PATH:+${PATH}:}${TOYS_PATH%:}"
+fi
+
+HAS_SUSFS="$(command -v ksu_susfs)"
+
 err() {
 	[ ! -f "$MODDIR/err" ] && cp "$MODDIR/module.prop" "$MODDIR/err"
 	sed -i "s/^des.*/description=⚠️ Needs reflash: '${1}'/g" "$MODDIR/module.prop"
@@ -41,7 +49,10 @@ run() {
 		err "apk not found"
 		return
 	fi
-	mount -o bind "$RVPATH" "$BASEPATH/base.apk"
+	$HAS_SUSFS && ksu_susfs add_sus_kstat "$BASEPATH"
+	[ -n "$BASEPATH" ] && mount -o bind "$RVPATH" "$BASEPATH/base.apk"
+	$HAS_SUSFS && ksu_susfs update_sus_kstat "$BASEPATH"
+	$HAS_SUSFS && ksu_susfs add_sus_mount "$BASEPATH"
 	am force-stop "$PKG_NAME"
 	[ -f "$MODDIR/err" ] && mv -f "$MODDIR/err" "$MODDIR/module.prop"
 }
