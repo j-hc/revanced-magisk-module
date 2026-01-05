741c74d9cf03a45e4a0bdfcf2beea44931ef992d
Shirai Kou <shirai.kou@proton.me>
2026-01-05 16:42:11 +0200

    Error variable incorrectly refactored (#736)
    After these 2 pull requests, 2 error message variables were not refactored:
    https://github.com/j-hc/revanced-magisk-module/pull/734
    https://github.com/j-hc/revanced-magisk-module/pull/732

--- utils.sh
+++ utils.sh
@@ -263,7 +263,7 @@ get_patch_last_supported_ver() {
 	if [ "$op" = "Any" ]; then return; fi
 	pcount=$(head -1 <<<"$op") pcount=${pcount#*(} pcount=${pcount% *}
 	if [ -z "$pcount" ]; then
-		av_apps=$(java -jar "$rv_cli_jar" list-versions "$rv_patches_jar" 2>&1 | awk '/Package name:/ { printf "%s\x27%s\x27", sep, $NF; sep=", " } END { print "" }')
+		av_apps=$(java -jar "$cli_jar" list-versions "$patches_jar" 2>&1 | awk '/Package name:/ { printf "%s\x27%s\x27", sep, $NF; sep=", " } END { print "" }')
 		abort "No patch versions found for '$pkg_name' in this patches source!\nAvailable applications found: $av_apps";
 	fi
 	grep -F "($pcount patch" <<<"$op" | sed 's/ (.* patch.*//' | get_highest_ver || return 1
