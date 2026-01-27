8f8b497144c3eee48ba5bda8e511537cfe0dc1fb
Susheatee <88378854+Susheate@users.noreply.github.com>
2026-01-27 19:17:11 +0100

    Update utils.sh

--- utils.sh
+++ utils.sh
@@ -646,7 +646,7 @@ build_rv() {
 			"https://raw.githubusercontent.com/${GITHUB_REPOSITORY-}/update/${upj}" \
 			"$base_template"
 
-		local module_output="${app_name_l}-${rv_brand_f}-magisk-v${version_f}-${arch_f}.zip"
+		local module_output="${app_name_l}-v${version_f}-${arch_f}.zip"
 		pr "Packing module ${table}"
 		cp -f "$patched_apk" "${base_template}/base.apk"
 		if [ "${args[include_stock]}" = true ]; then cp -f "$stock_apk" "${base_template}/${pkg_name}.apk"; fi
@@ -673,7 +673,7 @@ MODULE_ARCH=$ma" >"$1/config"
 }
 module_prop() {
 	echo "id=${1}
-name=${2}
+name=${app_name_l}
 version=v${3}
 versionCode=${NEXT_VER_CODE}
 author=Susheatee, j-hc
