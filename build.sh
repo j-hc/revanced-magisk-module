0c68945cd37f69b29df840ff26e4365ce1d561ed
Susheatee <88378854+Susheate@users.noreply.github.com>
2026-01-27 18:11:19 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -166,8 +166,8 @@ wait
 rm -rf temp/tmp.*
 if [ -z "$(ls -A1 "${BUILD_DIR}")" ]; then abort "All builds failed."; fi
 
-log "\nInstall [Microg](https://github.com/ReVanced/GmsCore/releases) for non-root YouTube and YT Music APKs"
 log "Use [zygisk-detach](https://github.com/j-hc/zygisk-detach) to detach root ReVanced YouTube and YT Music from Play Store"
+log "\n[rvx-morphe-module-builder](https://github.com/Susheate/rvx-morphe-module-builder)\n"
 log "\n[revanced-magisk-module](https://github.com/j-hc/revanced-magisk-module)\n"
 log "$(cat "$TEMP_DIR"/*/changelog.md)"
 
