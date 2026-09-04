8454b431036b62dfb5c65654453823843309646a
j-hc <j-hc@users.noreply.github.com>
2026-09-04 15:56:17 +0300

    remove obsolote flag

--- utils.sh
+++ utils.sh
@@ -582,7 +582,7 @@ patch_apk() {
 	cli_name=$(basename "$cli_jar")
 	if [ "${cli_name::8}" = revanced ]; then cmd+=" -b"; fi
 
-	if [ "$OS" = Android ]; then cmd+=" --custom-aapt2-binary='${AAPT2}'"; fi
+	# if [ "$OS" = Android ]; then cmd+=" --custom-aapt2-binary='${AAPT2}'"; fi
 	pr "$cmd"
 	if eval "$cmd"; then [ -f "$patched_apk" ]; else
 		rm "$patched_apk" 2>/dev/null || :
